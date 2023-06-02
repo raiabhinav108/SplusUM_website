f020390b338ccea6ae39c7d7c3fe725ab85fbf09
MANGALAM RAJ <mangalamraj@MANGALAMs-MacBook-Air.local>
2023-06-03 00:38:35 +0530

    New message for the combined commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import SingleProject from "./pages/SingleProj";
 import MultipleProj from "./pages/MultiProj";
+import ContactUs from "./pages/ContactUs";
+
 
 
 
@@ -12,6 +14,7 @@ function App() {
      <Route exact path="/" element={<Home/>}></Route>
      <Route exact path="/single" element={<SingleProject/>}></Route>
      <Route exact path="/multi" element={<MultipleProj/>}></Route>
+     <Route exact path="/contact" element={<ContactUs/>}></Route>
     </Routes>
       
     
