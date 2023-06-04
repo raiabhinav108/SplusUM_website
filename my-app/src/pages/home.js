f99650e5766e9187830ee7b55ade2aab3dc50db3
MANGALAM RAJ <mangalamraj@MANGALAMs-MacBook-Air.local>
2023-06-05 01:40:28 +0530

    preloaderdone

--- my-app/src/pages/home.js
+++ my-app/src/pages/home.js
@@ -7,11 +7,12 @@ import Projects from "../components/projects/Projects";
 import About from "../components/about/About";
 import ImageSlider from "../components/ImageSlider/ImageSlider";
 import OurServices from "../components/ourServices/OurServices";
-
+import { useState,useEffect } from "react";
 import Footer from "../components/footer/Footer";
 import News from "../components/news/News";
 import Team from "../components/team/Team";
 import Contact from "../components/contact/Contact";
+import ClipLoader from "react-spinners/ClipLoader";
 
 import Testimonials from "../components/testimonials/Testimonials";
 
@@ -20,25 +21,50 @@ import KeyWorks from "../components/keyWorks/KeyWorks";
 import Hello from "../components/hello/Hello";
 
 const Home = () => {
-  
+  const [loading, setloading] = useState(false);
+  useEffect(() => {
+    setloading(true);
+    setTimeout(() => {
+      setloading(false);
+    }, 4000);
+  }, []);
   return (
     <div>
-      <Navbar />
+      {
+        loading ? 
+        <div className="loader_mr">
+        <ClipLoader
+          color={"#000000"}
+          loading={loading}
+          size={50}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        /><div className="blink_me">
+        
+            S+UM
+        </div>
+        </div>
+        :
+        <div>
+        <Navbar />
 
-      <Slider />
+<Slider />
 
-      <Projects />
-      <Hello/>
-      <About />
+<Projects />
+<Hello/>
+<About />
 
-      <ImageSlider />
-      <AboutSub />
-      <OurServices />
-      <KeyWorks/>
-      <Team />
-      <Testimonials />
-      <News />
-      <Footer />
+<ImageSlider />
+<AboutSub />
+<OurServices />
+<KeyWorks/>
+<Team />
+<Testimonials />
+<News />
+<Footer />
+        </div>
+      }
+      
       
     </div>
   );
