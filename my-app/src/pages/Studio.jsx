76d50bf65eae3cb693af6b1be65677e71c7c1f1b
lOstinsOul <99341814+lOstinsOul@users.noreply.github.com>
2023-06-05 11:44:41 +0530

    Process section created

--- my-app/src/pages/Studio.jsx
+++ my-app/src/pages/Studio.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/navbar/Navbar'
 import ImageMarquee from '../studioComponents/imageMarquee/ImageMarquee'
 import Intro from '../studioComponents/intro/Intro'
+import ParallaxEffect from '../studioComponents/parallaxEffect/ParallaxEffect'
 import './studio.css'
 const Studio = () => {
   return (
@@ -9,6 +10,7 @@ const Studio = () => {
       <Navbar/>
       <Intro/>
       <ImageMarquee/>
+      <ParallaxEffect/>
     </div>
   )
 }
